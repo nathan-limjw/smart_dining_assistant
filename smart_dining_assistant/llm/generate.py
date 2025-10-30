eb0e6036f63bac048955ff93764827ac6b243cd0
Jia Yi <gohjiayi2810@gmail.com>
2025-10-30 17:23:33 +0800

    add prompt to openai to return generated response

---
+++ smart_dining_assistant/llm/generate.py
@@ -0,0 +1,18 @@
+# send a prompt to OpenAI and returns the generated answer
+
+from openai import OpenAI # library to interact with OpenAI REST API, send request & receive response from GPT models
+import os
+
+client = OpenAI(api_key = os.getenv("OPENAI_API_KEY")) 
+
+def call_llm(system_prompt: str, user_prompt: str) -> str:
+    resp = client.chat.completions.create(
+        model = "gpt-4o-mini",
+        messages = [
+            {'role': "system", "content": system_prompt},
+            {'role': "user", "content": user_prompt}
+        ],
+        temperature = 0.7,
+        max_tokens = 150,
+    )
+    return resp.choices[0].message.content.strip()
