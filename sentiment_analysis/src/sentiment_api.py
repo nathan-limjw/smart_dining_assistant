c1c48d29b7f180d1bbcbd2f9a44eaf0636cbaa09
Jia Yi <gohjiayi2810@gmail.com>
2025-11-04 21:17:52 +0800

    Changed path to local from ronin

--- sentiment_analysis/src/sentiment_api.py
+++ sentiment_analysis/src/sentiment_api.py
@@ -2,7 +2,7 @@ from transformers import pipeline
 
 
 class SentimentAnalyzer:
-    def __init__(self, model_path="models/sentiment_model"):
+    def __init__(self, model_path="sentiment_analysis/models/sentiment_model"):
         """
         Loads the trained sentiment model
         """
@@ -38,7 +38,7 @@ class SentimentAnalyzer:
         added sentiment columns
         """
 
-        print(f"Analyzing all {len(reviews_df)} reviews...")
+        # print(f"Analyzing all {len(reviews_df)} reviews...")
         results = [self.analyze(text) for text in reviews_df["text"].tolist()]
 
         reviews_df["sentiment"] = [result["sentiment"] for result in results]
